68e534b67e8154e6b2c0f2676f72591a9ba121fc
cs-satyam <satyamwork4464@gmail.com>
2026-04-09 18:13:36 +0530

    Add majorityElement class with implementation to find the majority element in an array

---
+++ DSA Practice/src/dsa2_Arrays/majorityElement.java
@@ -0,0 +1,21 @@
+package dsa2_Arrays;
+
+public class majorityElement {
+    public static void main(String[] args) {
+        int a[]={2,2,1,1,1,2,2};
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(a[i]==a[j]){
+                    count++;
+                }
+            }
+            if(count>n/2){
+                System.out.println("majority element: "+a[i]);
+                break;
+            }
+        }
+    }
+    
+}
