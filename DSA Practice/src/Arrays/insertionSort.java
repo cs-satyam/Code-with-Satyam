f0092add757aa366c63b3c7c5d69cab52a73b0e4
cs-satyam <satyamwork4464@gmail.com>
2026-04-09 11:01:54 +0530

    Implement insertionSort and selectionSort classes with main methods for testing

--- DSA Practice/src/Arrays/insertionSort.java
+++ DSA Practice/src/Arrays/insertionSort.java
@@ -1,4 +1,27 @@
 package Arrays;
 
+
+
 public class insertionSort {
+    static void sort(int[] arr){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0&&arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int arr[]={5,1,2,7,3};
+        sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
--- DSA Practice/src/Sorting_Algorithms/selectionSort.java
+++ DSA Practice/src/Sorting_Algorithms/selectionSort.java
@@ -1,4 +1,29 @@
 package Sorting_Algorithms;
 
 public class selectionSort {
+    static void sort(int a[]){
+        int n=a.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[min]){
+                    min=j;
+                }
+            }
+            if(i!=min) {
+                int temp = a[i];
+                a[i] = a[min];
+                a[min] = temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int arr[]={5,1,2,7,3};
+
+        sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
