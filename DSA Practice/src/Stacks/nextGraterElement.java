04c498ee2b8f82c9e672d235aa220e7bf31fd5b8
cs-satyam <satyamwork4464@gmail.com>
2026-04-27 00:30:39 +0530

    Implement nextGraterElement class with main method and logic

--- DSA Practice/src/Stacks/nextGraterElement.java
+++ DSA Practice/src/Stacks/nextGraterElement.java
@@ -1,4 +1,29 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class nextGraterElement {
+    public static void main(String[] args) {
+        int arr[]={6 ,8,0,1,3};
+        int n = arr.length;
+        int nxtele[]= new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&st.peek()<arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nxtele[i]=-1;
+            }else{
+                nxtele[i]=st.peek();
+            }
+            st.push(arr[i]);
+
+        }
+        for(int i:nxtele){
+            System.out.print(i+" ");
+        }
+    }
 }
