2bc573960b90880b13f3bb4b37e22e7705d22ea9
cs-satyam <satyamwork4464@gmail.com>
2026-04-26 20:16:52 +0530

    Implement reverseStringUsingStack class with reverse method and main function

--- DSA Practice/src/Stacks/reverseStringUsingStack.java
+++ DSA Practice/src/Stacks/reverseStringUsingStack.java
@@ -1,4 +1,30 @@
 package Stacks;
-
+import java.util.*;
 public class reverseStringUsingStack {
-}
+        public String reverse(String S) {
+            Stack<Character> s = new Stack<>();
+
+            for (char i : S.toCharArray()) {
+                s.push(i);
+            }
+
+            StringBuilder reversed = new StringBuilder();
+
+            while (!s.isEmpty()) {
+                reversed.append(s.pop());
+            }
+
+            return reversed.toString();
+        }
+
+        public static void main(String[] args) {
+            reverseStringUsingStack obj = new reverseStringUsingStack();
+
+            String str = "Satyam";
+            String result = obj.reverse(str);
+
+            System.out.println("Original String: " + str);
+            System.out.println("Reversed String: " + result);
+        }
+    }
+
