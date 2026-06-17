c379fac6e3f153632efd5388b38a8813b6d2c4bd
cs-satyam <satyamwork4464@gmail.com>
2026-06-17 20:16:53 +0530

    Lambda Expression

--- Java_FullStack/src/Lamda_expression/Nestedclasses/Demo0.java
+++ Java_FullStack/src/Lamda_expression/Nestedclasses/Demo0.java
@@ -5,19 +5,34 @@ class A {
     public void show(){
         System.out.println("A in show");
     }
+    public void display(){
+        System.out.println("b in display");
+    }
 }
 class B extends A{
     public void show(){
         System.out.println("B in show");
     }
+
 }
 public class Demo0 {
     public static void main(String[] args) {
       A a = new A(){
-        public void show(){
+          //we can't re-use it
+        public void show(){   // Anonymous inner class
+
             System.out.println("changing in show");
+        }
+        public void display(){
+            System.out.println("changing in display");
         }
       };
       a.show();
+      a.display();
+
+      // anonymous object
+//         can't re-use
+      new B().show();
+
     }
 }
