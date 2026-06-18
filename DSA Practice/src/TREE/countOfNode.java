1ce1c82d9e30a7f06e6dda22581ffc79692e6d43
cs-satyam <satyamwork4464@gmail.com>
2026-06-18 20:07:38 +0530

    Tree count node

--- DSA Practice/src/TREE/countOfNode.java
+++ DSA Practice/src/TREE/countOfNode.java
@@ -2,20 +2,25 @@ package TREE;
 
 public class countOfNode {
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.left.left = new Node(3);
-        root.left.left.left = new Node(4);
-        root.left.left.left.left = new Node(5);
-        root.left.left.left.left.left = new Node(6);
-        root.left.left.left.left.left.left = new Node(7);
 
-        System.out.println(coutNodes(root));
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(2);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+
+        root.right.left = new Node(4);
+        root.right.right = new Node(3);
+
+
+        System.out.println(countNodes(root));
 
     }
-    static int coutNodes(Node root) {
+    static int countNodes(Node root) {
         if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
-        return 1 + Math.max(coutNodes(root.left), coutNodes(root.right));
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
