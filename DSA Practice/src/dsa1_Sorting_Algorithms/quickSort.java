8ca5311fba0735dd51539102da413c794eb9a1d5
cs-satyam <satyamwork4464@gmail.com>
2026-06-20 01:12:50 +0530

    Add quickSort.java implementation for quick sort algorithm

---
+++ DSA Practice/src/dsa1_Sorting_Algorithms/quickSort.java
@@ -0,0 +1,42 @@
+package dsa1_Sorting_Algorithms;
+
+import java.util.Arrays;
+
+public class quickSort {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
