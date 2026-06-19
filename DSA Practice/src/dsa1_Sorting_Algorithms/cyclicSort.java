9663fd23bbd917feab25ca891553aebbb425e2d2
cs-satyam <satyamwork4464@gmail.com>
2026-06-20 01:12:43 +0530

    Add cyclicSort.java implementation for cyclic sort algorithm

---
+++ DSA Practice/src/dsa1_Sorting_Algorithms/cyclicSort.java
@@ -0,0 +1,33 @@
+package dsa1_Sorting_Algorithms;
+
+import java.util.Arrays;
+
+public class cyclicSort {
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 1, 4};
+
+        cyclicSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
