45946d3b70e1df7402c2b1eb8f7b740400a4f40b
cs-satyam <satyamwork4464@gmail.com>
2026-06-28 23:45:30 +0530

    launchExceptionHandling1

---
+++ Java_FullStack/src/Exception_Handling/launchExceptionHandling1.java
@@ -0,0 +1,28 @@
+package Exception_Handling;
+
+import java.util.Scanner;
+
+public class launchExceptionHandling1 {
+    public  static void main(String args[]){
+        System.out.println("Connection -Eastablish");
+
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("kindly enter the Numerator for devision");
+            int n = sc.nextInt();
+
+            System.out.println("enter the value of Denominator for  Division");
+            int d = sc.nextInt();
+
+            int res = n / d;
+            System.out.println("Result " + res);
+
+        }
+        catch(Exception e){
+            System.out.println("Please enter a non zero Denominator  value");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Connection terminated");
+
+    }
+}
