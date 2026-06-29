a1a906b5ae515f8a221bb974753ab6fe51813bbe
cs-satyam <satyamwork4464@gmail.com>
2026-06-29 12:27:51 +0530

    Graph

--- DSA Practice/src/Graph/DFSGraph.java
+++ DSA Practice/src/Graph/DFSGraph.java
@@ -1,32 +1,46 @@
 package Graph;
 
+import java.util.ArrayList;
+
 public class DFSGraph {
 
-    public static void dfs(int[][] graph, int curr, boolean[] visited) {
-
-        System.out.print(curr + " ");
-        visited[curr] = true;
-
-        for (int j = 0; j < graph.length; j++) {
-            if (graph[curr][j] != 0 && !visited[j]) {
-                dfs(graph, j, visited);
-            }
-        }
-    }
 
     public static void main(String[] args) {
 
-        int V = 5;
-
         int[][] graph = {
                 {0, 5, 0, 0, 0},
-                {5, 0, 1, 3, 0},
-                {0, 1, 0, 1, 4},
-                {0, 3, 1, 0, 0},
-                {0, 0, 4, 0, 0}
+                {5, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 4},
+                {0, 0, 0, 4, 0}
         };
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        boolean[] visited = new boolean[V];
-        dfs(graph, 0, visited);
+
+
+        for(int i = 0; i < graph.length; i++){
+            adj.add(new ArrayList<>());
+            for(int j = 0; j < graph[0].length; j++){
+                if(graph[i][j] != 0){
+                    adj.get(i).add(j);
+                }
+            }
+        }
+        System.out.println(adj);
+
+        boolean[] visited = new boolean[graph.length];
+        DFS(adj,visited,0);
+    }
+
+    public static void DFS(ArrayList<ArrayList<Integer>> adj, boolean [] visited,int curr){
+
+            visited[curr] = true;
+
+            for(int i = 0; i < adj.size(); i++){
+                if(!visited[i] && adj.get(curr).contains(i)){
+                    DFS(adj,visited,i);
+                }
+            }
+
     }
 }
