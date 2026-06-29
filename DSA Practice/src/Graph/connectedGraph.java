a3e53a48744b6e5a6520eb561200980e5f5ca0e8
cs-satyam <satyamwork4464@gmail.com>
2026-06-29 12:27:26 +0530

    Graph

---
+++ DSA Practice/src/Graph/connectedGraph.java
@@ -0,0 +1,58 @@
+package Graph;
+
+
+import java.util.ArrayList;
+
+public class connectedGraph {
+
+
+    public static void main(String[] args) {
+
+        int[][] graph = {
+                {0, 5, 0, 0, 0},
+                {5, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 4},
+                {0, 0, 0, 4, 0}
+        };
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+
+
+        for(int i = 0; i < graph.length; i++){
+            adj.add(new ArrayList<>());
+            for(int j = 0; j < graph[0].length; j++){
+                if(graph[i][j] != 0){
+                    adj.get(i).add(j);
+                }
+            }
+        }
+        System.out.println(adj);
+
+        boolean[] visited = new boolean[graph.length];
+        DFS(adj,visited,0);
+
+
+        for (boolean b : visited) {
+            if (!b) {
+                System.out.println("false");
+                return;
+            }
+        }
+        System.out.println("true");
+    }
+
+    public static void DFS(ArrayList<ArrayList<Integer>> adj, boolean [] visited,int curr){
+
+        visited[curr] = true;
+        System.out.print(adj.size() + " ");
+
+        for(int i = 0; i < adj.size(); i++){
+            if(!visited[i] && adj.get(curr).contains(i)){
+                DFS(adj,visited,i);
+            }
+        }
+
+
+    }
+}
