a53e5ccd9fdc08e94a750a9aebfb097fbd255f29
cs-satyam <satyamwork4464@gmail.com>
2026-06-30 12:11:11 +0530

    Update flood fill example graph and change fill color in main method

--- LeetCode practice/0733-flood-fill/main.java
+++ LeetCode practice/0733-flood-fill/main.java
@@ -2,11 +2,11 @@ public class main {
     public static void main(String[] args) {
         Q_07733 s = new Q_07733();
         int [][]graph={
-                {1,2,2,4},
-                {1,2,3,4},
-                {1,2,4,3},
+                {1,1,1,1},
+                {1,1,1,0},
+                {1,1,0,1},
         };
-       s.floodFill(graph,0, 0,1);
+       s.floodFill(graph,0, 0,2);
        for (int i = 0; i < graph.length; i++) {
             for (int j = 0; j < graph[i].length; j++) {
                 System.out.print(graph[i][j] + " ");
