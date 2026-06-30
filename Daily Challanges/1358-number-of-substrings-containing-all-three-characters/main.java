f3e118a04dc6ad83857655904e4991e3882203f2
cs-satyam <satyamwork4464@gmail.com>
2026-06-30 11:59:55 +0530

    Implement solution for "Number of Substrings Containing All Three Characters" and add README

--- Daily Challanges/1358-number-of-substrings-containing-all-three-characters/main.java
+++ Daily Challanges/1358-number-of-substrings-containing-all-three-characters/main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        Solution s= new Solution();
+      int   res=s.numberOfSubstrings("abcabc");
+        System.out.println(res);
+    }
 }
