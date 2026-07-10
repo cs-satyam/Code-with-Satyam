bfc4442520508156bcec82832c48974f8570c76d
cs-satyam <satyamwork4464@gmail.com>
2026-07-11 01:34:58 +0530

    binarySearch

---
+++ Infosys/src/BinarySearch/Sqrt_X_.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class Sqrt_X_ {
+    public static void main(String[] args) {
+        int n=4;
+        int x=sqrt(n);
+        System.out.println("Square root of "+n+" is : "+x);
+    }
+    static int sqrt(int x){
+        if(x==0||x==1){
+            return x;
+        }
+        long l=1;
+        long r=x/2;
+        while(l<=r){
+            long mid= l+(r-l)/2;
+            if(mid*mid==x){
+                return (int )mid;
+            } else if (mid*mid<x) {
+                l=mid+1;
+
+            }else r=mid-1;
+        }
+        return (int)r;
+    }
+}
