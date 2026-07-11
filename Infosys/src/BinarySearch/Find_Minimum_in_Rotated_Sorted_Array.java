3e9262d9683e48c32248c90223ecd98b29bb33ef
cs-satyam <satyamwork4464@gmail.com>
2026-07-11 23:30:56 +0530

    binarySearch

---
+++ Infosys/src/BinarySearch/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class Find_Minimum_in_Rotated_Sorted_Array {
+    public static void main(String[] args) {
+        int a[]={3,4,5,1,2};
+        System.out.println( findMin(a));
+    }
+    static int findMin(int a[]){
+     int l=0;
+     int r=a.length-1;
+     while(l<r){
+         int mid = l+(r-l)/2;
+         if(a[mid]>a[r]){
+             l=mid+1;
+         }else{
+             r=mid-1;
+         }
+     }
+     return a[l];
+    }
+}
