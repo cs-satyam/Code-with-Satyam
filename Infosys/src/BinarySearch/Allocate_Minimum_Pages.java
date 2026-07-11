c6543db18fb724f128fa71e93c2902e7d0026a1c
cs-satyam <satyamwork4464@gmail.com>
2026-07-11 22:08:41 +0530

    Refactor and reorganize binary search problems
    - Removed implementations and README files for "Search in Rotated Sorted Array II" and "Split Array Largest Sum".
    - Added new implementations and README files for "Search in Rotated Sorted Array", "Find First and Last Position of Element in Sorted Array", and "Split Array Largest Sum".
    - Introduced new solutions for "Number of Strings that Appear as Substrings in Word" and "Find the Safest Path in a Grid".

--- Infosys/src/BinarySearch/Allocate_Minimum_Pages.java
+++ Infosys/src/BinarySearch/Allocate_Minimum_Pages.java
@@ -1,4 +1,45 @@
 package BinarySearch;
 
 public class Allocate_Minimum_Pages {
+    public static void main(String[] args) {
+        int a[]= {25,46,28,49,24};
+        int k=4;
+       long x= findPages(a,k);
+        System.out.println(x);
+
+    }
+    static int findPages(int a[],int k){
+        long l=0;
+        long h=0;
+        for(int i:a){
+            l= Math.max(l,i);
+            h+=i;
+        }
+        while(l<=h){
+            long mid= l+(h-l)/2;
+            if(countStudent(a,mid)>k){
+                l=mid+1;
+
+            }else{
+                h=mid-1;
+            }
+
+
+        }
+        return (int) l;
+    }
+    static int countStudent(int a[],long p){
+        int studentCount=1;
+        int pages=0;
+        for(int i:a){
+            if(pages+i<=p){
+                pages += i;
+            }
+            else{
+                studentCount++;
+                pages =i;
+            }
+        }
+        return studentCount;
+    }
 }
