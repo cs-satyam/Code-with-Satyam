a0c1a5aa93ba9f33bc108e99078d583e817017a0
cs-satyam <satyamwork4464@gmail.com>
2026-07-11 15:02:06 +0530

    binarySearch

---
+++ Infosys/src/BinarySearch/Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+public class Search_in_Rotated_Sorted_Array {
+    public static void main(String[] args) {
+        int a[]={4,5,6,7,0,1,2};
+        int target=0;
+      int x =  search(a,target);
+        System.out.println("Search_in_Rotated_Sorted_Array: "+x);
+
+    }
+    static int search(int a[],int t){
+        int l=0;
+        int r=a.length-1;
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            if(a[mid]==t){
+                return mid;
+            }
+            if(a[l]<=a[mid]){
+                if(a[l]<=t&& t<a[r]){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+                }
+            }else if(a[l]>a[mid]){
+                if(a[mid]<t && t <=a[r]){
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
