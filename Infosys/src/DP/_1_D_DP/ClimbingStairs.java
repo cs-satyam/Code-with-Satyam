59a3df45a61d57625286c6e7538842eb19a35faf
cs-satyam <satyamwork4464@gmail.com>
2026-07-13 01:45:47 +0530

    1 d dp

---
+++ Infosys/src/DP/_1_D_DP/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package DP._1_D_DP;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(climb(n));
+    }
+    static int climb(int n){
+        if(n<=3) return n;
+        int dp[]= new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        for(int i=4;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
---
+++ Infosys/src/DP/_1_D_DP/HouseRobber.java
@@ -0,0 +1,24 @@
+package DP._1_D_DP;
+
+public class HouseRobber {
+    public static void main(String[] args) {
+        int a[]={5,15,6,20,22};
+        System.out.println(rob(a));
+    }
+    static int rob(int nums[]){
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int dp[] = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i <n; i++) {
+            int include = dp[i - 2] + nums[i];
+            int exclude = dp[i - 1];
+            dp[i] = Math.max(include, exclude);
+        }
+
+        return dp[n-1];
+    }
+}
