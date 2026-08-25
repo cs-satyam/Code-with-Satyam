a34a77a9817e7320fc0532fba7c38e1f6fad453a
satyam kumar <satyamwork4464@gmail.com>
2026-08-25 18:52:35 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.45%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3)
+            return n;
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp,-1);
+        return f(n, dp);
+    }
+
+    int f(int n, int dp[]) {
+        if (n <= 3)
+            return n;
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+    }
+}
