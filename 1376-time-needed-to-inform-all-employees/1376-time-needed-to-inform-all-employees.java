e6885b999d24291432099c3440ef195bd2310f46
satyam kumar <satyamwork4464@gmail.com>
2026-08-28 11:11:39 +0530

    Time: 124 ms (10.03%), Space: 123.5 MB (68.49%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != -1) {
+                adj.get(manager[i]).add(i);
+            }
+        }
+        System.out.println(adj);
+        return dfs(headID, 0, adj, informTime);
+    }
+
+    int dfs(int headID, int currTime, List<List<Integer>> adj, int[] informTime) {
+                if(adj.get(headID).isEmpty()){
+                    return currTime;
+                }
+
+                int maxTime=0;
+                int newTime = currTime + informTime[headID] ;
+
+                for(int e:adj.get(headID)){
+                        maxTime= Math.max(maxTime,dfs(e, newTime, adj, informTime));
+                }
+                return maxTime;
+    }
+}
